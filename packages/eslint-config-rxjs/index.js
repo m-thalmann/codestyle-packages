d68ec8a744fe26f8aeec10a5821b77689ddc6d4b
Matthias Thalmann <matthiasthalmann1@hotmail.de>
2024-05-10 11:20:46 +0200

    feat(eslint-config-rxjs): add missing rules

--- packages/eslint-config-rxjs/index.js
+++ packages/eslint-config-rxjs/index.js
@@ -19,6 +19,7 @@ module.exports = [
           strict: true,
           types: {
             '^EventEmitter$': false,
+            '^Store$': false,
           },
           variables: true,
         },
@@ -30,11 +31,43 @@ module.exports = [
       // https://github.com/cartant/eslint-plugin-rxjs/blob/main/docs/rules/no-create.md
       'rxjs/no-create': ['error'],
 
+      // https://github.com/cartant/eslint-plugin-rxjs/blob/main/docs/rules/no-exposed-subjects.md
+      'rxjs/no-exposed-subjects': ['warn', { allowProtected: true }],
+
+      // https://github.com/cartant/eslint-plugin-rxjs/blob/main/docs/rules/no-ignored-takewhile-value.md
+      'rxjs/no-ignored-takewhile-value': ['warn'],
+
+      // https://github.com/cartant/eslint-plugin-rxjs/blob/main/docs/rules/no-index.md
+      'rxjs/no-index': ['error'],
+
+      // https://github.com/cartant/eslint-plugin-rxjs/blob/main/docs/rules/no-internal.md
+      'rxjs/no-internal': ['error'],
+
       // https://github.com/cartant/eslint-plugin-rxjs/blob/main/docs/rules/no-nested-subscribe.md
       'rxjs/no-nested-subscribe': ['error'],
 
       // https://github.com/cartant/eslint-plugin-rxjs/blob/main/docs/rules/no-sharereplay.md
       'rxjs/no-sharereplay': ['warn', { allowConfig: true }],
+
+      // https://github.com/cartant/eslint-plugin-rxjs/blob/main/docs/rules/no-subject-value.md
+      'rxjs/no-subject-value': ['warn'],
+
+      // https://github.com/cartant/eslint-plugin-rxjs/blob/main/docs/rules/no-unbound-methods.md
+      'rxjs/no-unbound-methods': ['error'],
+
+      // https://github.com/cartant/eslint-plugin-rxjs/blob/main/docs/rules/no-unsafe-subject-next.md
+      'rxjs/no-unsafe-subject-next': ['warn'],
+
+      // https://github.com/cartant/eslint-plugin-rxjs/blob/main/docs/rules/no-unsafe-takeuntil.md
+      'rxjs/no-unsafe-takeuntil': [
+        'error',
+        {
+          alias: ['takeUntilDestroyed'],
+        },
+      ],
+
+      // https://github.com/cartant/eslint-plugin-rxjs/blob/main/docs/rules/throw-error.md
+      'rxjs/throw-error': ['warn'],
     },
   },
 ];
